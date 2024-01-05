c81da6e4dbb876ae9a0203254054c0b664a95f7f
Klif <sang.wicklife@student.moringaschool.com>
2024-01-05 13:32:47 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Your code here
+function moveDodgerLeft() {
+    let currentPosition = dodger.style.left;
+    currentPosition = parseInt(currentPosition) || 0;
+    // adjust the decrement as needed
+    let newPosition = currentPosition - 180; 
+    dodger.style.left = newPosition + 'px';
+  }
+
+function moveDodgerRight() {
+    let currentPosition = dodger.style.left;
+    currentPosition = parseInt(currentPosition) || 0;
+  // adjust the increment as needed
+    let newPosition = currentPosition + 180; 
+    dodger.style.left = newPosition + 'px';
+  }
+
+//   moveDodgerLeft()
+//   moveDodgerRight();
